4cfad12bbb743c4b8f0bd2d87dbf2846b606c7f5
Jeffrey Heer <jeffrey.heer@gmail.com>
2015-05-06 08:47:45 -0700

    Create gh-pages branch.

--- src/import/formats/csv.js
+++
@@ -1,6 +0,0 @@
-var d3 = require('d3');
-
-module.exports = function(data, format) {
-  var d = d3.csv.parse(data ? data.toString() : data);
-  return d;
-};
--- src/import/formats/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  json: require('./json'),
-  csv: require('./csv'),
-  tsv: require('./tsv'),
-  topojson: require('./topojson'),
-  treejson: require('./treejson')
-};
