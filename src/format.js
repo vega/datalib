1e4d686720224c6dfd3563c20e367ea757ffb685
Jeffrey Heer <jeffrey.heer@gmail.com>
2015-07-29 21:54:34 -0700

    Add auto-formatters.

--- src/format.js
+++ src/format.js
@@ -1,17 +1,108 @@
-var d3_numbers = require('d3-format'),
-    d3_times = require('d3-time-format'),
-    numbers = d3_numbers, // inits to EN-US
-    times = d3_times;
+var d3_time = require('d3-time'),
+    d3_timeF = require('d3-time-format'),
+    d3_numberF = require('d3-format'),
+    numberF = d3_numberF, // defaults to EN-US
+    timeF = d3_timeF;     // defaults to EN-US
+
+function timeAutoFormat(utc) {
+  var f = utc ? timeF.utcFormat : timeF.format,
+      formatMillisecond = f('.%L'),
+      formatSecond = f(':%S'),
+      formatMinute = f('%I:%M'),
+      formatHour = f('%I %p'),
+      formatDay = f('%a %d'),
+      formatWeek = f('%b %d'),
+      formatMonth = f('%B'),
+      formatYear = f('%Y');
+
+  return function(date) {
+    var d = +date;
+    return (d3_time.second(date) < d ? formatMillisecond
+        : d3_time.minute(date) < d ? formatSecond
+        : d3_time.hour(date) < d ? formatMinute
+        : d3_time.day(date) < d ? formatHour
+        : d3_time.month(date) < d ?
+          (d3_time.week(date) < d ? formatDay : formatWeek)
+        : d3_time.year(date) < d ? formatMonth
+        : formatYear)(date);
+  };
+}
+
+var e10 = Math.sqrt(50),
+    e5 = Math.sqrt(10),
+    e2 = Math.sqrt(2);
+
+function intervals(domain, count) {
+  if (count == null) count = 10;
+
+  var start = domain[0],
+      stop = domain[domain.length - 1];
+
+  if (stop < start) { error = stop; stop = start; start = error; }
+
+  var span = stop - start,
+      step = Math.pow(10, Math.floor(Math.log(span / count) / Math.LN10)),
+      error = span / count / step;
+
+  // Filter ticks to get closer to the desired count.
+  if (error >= e10) step *= 10;
+  else if (error >= e5) step *= 5;
+  else if (error >= e2) step *= 2;
+
+  // Round start and stop values to step interval.
+  return [
+    Math.ceil(start / step) * step,
+    Math.floor(stop / step) * step + step / 2, // inclusive
+    step
+  ];
+}
+
+function numberAutoFormat(domain, count, f) {
+  var range = intervals(domain, count);
+  if (f == null) {
+    f = ',.' + d3_numberF.precisionFixed(range[2]) + 'f';
+  } else {
+    switch (f = d3_numberF.formatSpecifier(f), f.type) {
+      case 's': {
+        var value = Math.max(Math.abs(range[0]), Math.abs(range[1]));
+        if (f.precision == null) f.precision = d3_numberF.precisionPrefix(range[2], value);
+        return numberF.formatPrefix(f, value);
+      }
+      case '':
+      case 'e':
+      case 'g':
+      case 'p':
+      case 'r': {
+        if (f.precision == null) f.precision = d3_numberF.precisionRound(range[2], Math.max(Math.abs(range[0]), Math.abs(range[1]))) - (f.type === 'e');
+        break;
+      }
+      case 'f':
+      case '%': {
+        if (f.precision == null) f.precision = d3_numberF.precisionFixed(range[2]) - (f.type === '%') * 2;
+        break;
+      }
+    }
+  }
+  return numberF.format(f);
+}
 
 module.exports = {
   // Update number formatter to use provided locale configuration.
   // For more see https://github.com/d3/d3-format
-  numberLocale: function(l) { numbers = d3_numbers.localeFormat(l); },
-  number:       function(f) { return numbers.format(f); },
+  numberLocale: function(l) { numberF = d3_numberF.localeFormat(l); },
+  number:       function(f) { return numberF.format(f); },
+  numberPrefix: function(f) { return numberF.formatPrefix(f); },
 
   // Update time formatter to use provided locale configuration.
   // For more see https://github.com/d3/d3-time-format
-  timeLocale:   function(l) { times = d3_times.localeFormat(l); },
-  time:         function(f) { return times.format(f); },  
-  utc:          function(f) { return times.utcFormat(f); }
+  timeLocale:   function(l) { timeF = d3_timeF.localeFormat(l); },
+  time:         function(f) { return timeF.format(f); },  
+  utc:          function(f) { return timeF.utcFormat(f); },
+
+  // automatic formatting functions
+  auto: {
+    number:   numberAutoFormat,
+    time:     function() { return timeAutoFormat(0); },
+    utc:      function() { return timeAutoFormat(1); }
+  }
 };
